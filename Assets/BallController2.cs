f969d1630bdbdb35d0f4c9d5c345f621a4c39b36
ruki hamaguchi <king-king-7@ezweb.ne.jp>
2017-10-20 17:59:47 +0900

    スコアの表示
    スコア加算のスクリプトを書きスコアを表示できるようにしました。

---
+++ Assets/BallController2.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BallController2 : MonoBehaviour {
+
+	public Text scoreLabel;
+
+	private int score = 0;
+
+	// Use this for initialization
+	void Start () {
+
+		score = 0;
+
+
+		SetScore ();
+
+	}
+	
+	void OnCollisionEnter(Collision collision) {
+
+		if (collision.gameObject.tag == "SmallStarTag") {
+
+			score += 5;
+
+		} else if (collision.gameObject.tag == "LargeStarTag") {
+
+			score += 20;
+
+		} else if (collision.gameObject.tag == "SmallCloudTag") {
+
+			score += 10;
+
+		} else if (collision.gameObject.tag == "LargeCloudTag") {
+
+			score += 30;
+		}
+
+		SetScore ();
+	}
+
+	void SetScore() {
+
+		scoreLabel.text = "Score :" + score;
+	}
+}
